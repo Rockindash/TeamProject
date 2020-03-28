d4e52bd4574c955575a6cfc4b75bc42cda8561ba
Anagh Mehran <anagh07@gmail.com>
2020-03-28 13:54:34 -0700

    Initialize express server

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+      "mongodb+srv://anagh:iddqdidkfa@itemslist-f6pab.mongodb.net/test?retryWrites=true&w=majority"
+  };
+  
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const mongoURI = require("./config/keys");
+
+// Use express body-parser
+app.use(express.json());
+
+// Connect with mongo cloud db
+const db = mongoURI.mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+// Set a temporary default route
+app.get("/", (req, res) => {
+  res.json({ msg: "this is a temporary server" });
+});
+
+// Initialize server on port 5000
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}...`);
+});
