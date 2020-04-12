050c3b70f7204cd59e77ebac930082a86c87b0dd
Ascension1110 <ascension1110@gmail.com>
2020-04-12 16:04:02 -0400

    testing module implemented for GET/POST routes

---
+++ tests/routes.test.js
@@ -0,0 +1,42 @@
+const request = require("supertest")
+var app = require("../server")
+
+describe("Items api route", () => {
+    it("GET items ", function(done) {
+      request(app)
+        .get("/api/items")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200, done)
+    })
+
+    it("should send data to the db", () => {
+        request(app)
+          .post("/api/items")
+          .send({
+            inDiningList: ["Milk", "Oats"],
+            inDineSelectedState: [true, false],
+            outDiningList: ["Burger"],
+            outDineSelectedState: [true],
+            milkGroup: ["Milk"],
+            vegGroup: [],
+            grainGroup: ["Oats"],
+            meatGroup: [],
+            milkGroupUnit: ["Cup"],
+            grainGroupUnit: ["Gms"],
+            meatGroupUnit: [],
+            vegGroupUnit: [],
+            milkGroupQuantity: ["1"],
+            vegGroupQuantity: [],
+            grainGroupQuantity: ["20"],
+            meatGroupQuantity: [],
+            milkSum: 200,
+            vegSum: 0,
+            grainSum: 0,
+            meatSum: 0,
+          })
+          .expect(200, {
+            msg: "updated",
+          })
+      })
+    })
