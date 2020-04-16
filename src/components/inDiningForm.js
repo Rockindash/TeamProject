c2056bf2c0e783588298d526e8781ca7026206b1
Amol Kumar <Ezeabasili@yahoo.co.uk>
2020-04-15 20:41:04 -0400

    Date Picker Added

--- src/components/inDiningForm.js
+++ src/components/inDiningForm.js
@@ -10,17 +10,32 @@ import UnitDropdown from '../components/unitDropdown';
 
 class InDiningForm extends React.Component {
 
+    state = {
+      startDate: new Date(),
+      currentDay: new Date().getDate(),
+      currentMonth: new Date().getMonth(),
+      currentYear: new Date().getFullYear()
+    };
+
+    handleChange = date => {
+      this.setState({
+        startDate: date
+      });
+    };
+
     closeForm = () => {
         this.props.handleForm(false)
     };
 
     handleAdd = e => {
-        this.props.addItem(this.props.field.inDineName[this.props.items.selectedIndex])
-        this.props.addTime(this.props.field.currentDate)
-        this.props.addUnit(this.props.items.unitName[this.props.items.selectedUnitIndex])
-        this.props.addQuantity(this.props.field.quantity)
-        this.props.addSelectedState(false)
-        this.props.handleForm(false)
+      let dateFormat = `${this.state.startDate.getMonth()+1}/${this.state.startDate.getDate()}/${this.state.startDate.getFullYear()}`
+
+      this.props.addItem(this.props.field.inDineName[this.props.items.selectedIndex])
+      this.props.addDate(dateFormat)
+      this.props.addUnit(this.props.items.unitName[this.props.items.selectedUnitIndex])	
+      this.props.addQuantity(this.props.field.quantity)  
+      this.props.addSelectedState(false)
+      this.props.handleForm(false)  
     };
 
     handleQuantity = e => {
@@ -38,8 +53,15 @@ class InDiningForm extends React.Component {
               <h2>(In Dining)</h2>
               
               <TimeWrapper>
-                  <h1>Time</h1>
-                  <DropdownWrapperTime><p>{this.props.field.currentDate}</p></DropdownWrapperTime>
+                  <h1>Date</h1>
+                  <DropdownWrapperTime>
+                    <p>
+                      <DatePicker
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                      />
+                    </p>
+                  </DropdownWrapperTime>
               </TimeWrapper>
               <QuantityWrapper>
                   <h1>Quantity</h1>
@@ -82,6 +104,12 @@ const mapDispatchToProps = dispatch => {
       dispatch({
         type: 'form',
         payload: isEnabled,
+      });
+    },
+    addDate: date => {
+      dispatch({
+          type: 'inDineItemDate',
+          payload: date,
       });
     },
     addItem: items => {
@@ -275,6 +303,7 @@ const DropdownWrapperTime = styled.div`
   position: absolute;
   width: 400px;
   height: 40px;
+  z-index: 95;
   border-radius: 5px;
   margin-top: 250px;
   border: 2px solid black;
@@ -287,6 +316,9 @@ const DropdownWrapperTime = styled.div`
     font-weight: bold;
     font-family: Arial;
     color: black;
+  }
+  input{
+    border: none;
   }
 `;
 
